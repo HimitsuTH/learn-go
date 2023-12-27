3eb2c20a3e6f057240096c2166be12d9026f9ff7
Chinnawich (Shin) Ampai <am.chinnawich@gmail.com>
2023-12-27 14:05:37 +0700

    condition if else switch 🎠

--- main.go
+++ main.go
@@ -2,15 +2,50 @@ package main
 
 import (
 	"fmt"
-
-	greetings "github.com/HimitsuTH/learn-go/sayHello"
-	"github.com/google/uuid"
 )
 
 func main() {
-	id := uuid.New()
-	fmt.Println("Hello world 3")
-	fmt.Printf("UUID: %s\n", id)
-	greetings.SayHello()
-	greetings.SayTest()
+	a := 20
+	b := 30
+
+	// if eles
+	if a == b {
+		fmt.Println(10)
+	} else {
+		fmt.Println("Number must match.")
+	}
+
+	DayOfWeek()
+
+	num1 := 5
+	num2 := 10
+
+	if sumNum := num1 + num2; sumNum >= 10 {
+		fmt.Println("sumNum more than 10")
+	}
+}
+
+func DayOfWeek() {
+
+	// SWitch case
+	var dayOfWeek = 3
+
+	switch dayOfWeek {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Invalid Day")
+	}
 }
