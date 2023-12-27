e3cf815459df39f26ce955e2cec09b28e180f4b6
Chinnawich (Shin) Ampai <am.chinnawich@gmail.com>
2023-12-27 13:43:13 +0700

    Variable 🎨

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
-
-	greetings "github.com/HimitsuTH/learn-go/sayHello"
-	"github.com/google/uuid"
 )
 
 func main() {
-	id := uuid.New()
-	fmt.Println("Hello world 3")
-	fmt.Printf("UUID: %s\n", id)
-	greetings.SayHello()
-	greetings.SayTest()
+
+	fullName := `Chinnawich Ampai`
+	age := 22
+
+	fmt.Printf("Hello : %s\n", fullName)
+	fmt.Printf("age : %d\n", age)
+
+	fullName = "Shin"
+	fmt.Println(fullName)
+
+	//🚀 Type of Variable
+	var booleanVar bool = true
+	var intVar int = 10
+	var floatVar float64 = 3.14
+	var stringVar string = "Hello Go"
+
+	fmt.Println("Boolean:", booleanVar)
+	fmt.Println("Integer:", intVar)
+	fmt.Println("Float:", floatVar)
+	fmt.Println("String:", stringVar)
 }
