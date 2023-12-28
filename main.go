1fd55291a1b3e22bd976c5b830b8772d7fb851ca
Chinnawich (Shin) Ampai <am.chinnawich@gmail.com>
2023-12-28 11:19:55 +0700

    Fucntion&Interface

--- main.go
+++ main.go
@@ -2,15 +2,98 @@ package main
 
 import (
 	"fmt"
-
-	greetings "github.com/HimitsuTH/learn-go/sayHello"
-	"github.com/google/uuid"
 )
 
+func sayHello(name string) {
+	fmt.Printf("Hello %s\n", name)
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+// Define the Student struct
+type Student struct {
+	firstName string
+	lastName  string
+}
+
+// Method with a receiver of type Student
+// This method returns the full name of the student
+func (s Student) FullName() string {
+	return s.firstName + " " + s.lastName
+}
+
+// Define a struct type
+type Rectangle struct {
+	Length float64
+	Width  float64
+}
+
+// Method with a receiver of type Rectangle
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Width
+}
+
+// ----------------------------------------------------------------
+// Speaker interface
+type Speaker interface {
+	Speak() string
+}
+
+// Dog struct
+type Dog struct {
+	Name string
+}
+
+// Dog's implementation of the Speaker interface
+func (d Dog) Speak() string {
+	return "Woof!"
+}
+
+// Person struct
+type Person struct {
+	Name string
+}
+
+// Person's implementation of the Speaker interface
+func (p Person) Speak() string {
+	return "Hello!"
+}
+
+// function that accepts Speaker interface
+func makeSound(s Speaker) {
+	fmt.Println(s.Speak())
+}
+
 func main() {
-	id := uuid.New()
-	fmt.Println("Hello world 3")
-	fmt.Printf("UUID: %s\n", id)
-	greetings.SayHello()
-	greetings.SayTest()
+	sayHello("Shin")
+
+	number1 := 3
+	number2 := 5
+	sumNumber := add(number1, number2)
+	fmt.Println(sumNumber)
+
+	student := Student{
+		firstName: "Shin",
+		lastName:  "TH",
+	}
+
+	// Call the FullName method on the Student instance
+	fullName := student.FullName()
+	fmt.Println("Full Name of the student:", fullName)
+
+	rect := Rectangle{Length: 10, Width: 5}
+
+	// Call the Area method on Rectangle instance
+	area := rect.Area()
+	fmt.Println("Area of rectangle:", area)
+
+	//#Interface
+	dog := Dog{Name: "Buddy"}
+	person := Person{Name: "Alice"}
+
+	makeSound(dog)
+	makeSound(person)
+
 }
